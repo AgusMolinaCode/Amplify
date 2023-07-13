ef54f8493be1a8cc450ca40c0581b1526293aa29
Agustin Molina <agustinmolina@MacBook-Air-de-Agustin.local>
2023-07-12 21:40:17 -0300

    1.1

--- components/AllProducts.tsx
+++ components/AllProducts.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createClient } from "contentful";
 import HomeProduct from "./HomeProduct";
 import Footer from "./Footer";
-import { useQuery, QueryClient, QueryClientProvider } from "react-query";
 
 interface Product {
   sys: {
@@ -31,17 +30,28 @@ interface Product {
 }
 
 const AllProducts = () => {
-  const queryClient = new QueryClient();
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState<Product[]>([]);
 
-  const { isLoading, data } = useQuery<Product[]>("products", async () => {
+  useEffect(() => {
     const client = createClient({
       space: "pixs9lk7954s",
       accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_KEY as string,
     });
 
-    const response = await client.getEntries<Product>();
-    return response.items;
-  });
+    client
+      .getEntries<Product>({
+        content_type: "alquiler",
+      })
+      .then((response) => {
+        setProducts(response.items);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
     <div className="">
@@ -54,24 +64,15 @@ const AllProducts = () => {
       ) : (
         <div>
           <div className="flex flex-wrap flex-initial justify-center items-center mx-auto gap-4 px-2 md:px-10 mt-14 pb-10 ">
-            {data?.map((product) => (
+            {products.map((product) => (
               <HomeProduct key={product.sys.id} product={product} />
             ))}
           </div>
           <Footer />
         </div>
       )}
-      
     </div>
   );
 };
 
-const AllProductsWrapper = () => {
-  return (
-    <QueryClientProvider client={new QueryClient()}>
-      <AllProducts />
-    </QueryClientProvider>
-  );
-};
-
-export default AllProductsWrapper;
+export default AllProducts;
